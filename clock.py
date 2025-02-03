dd5464887590e90561a242805d514803b40d9394
JoanDing08 <j.okia.ding08@gmail.com>
2025-02-03 16:08:49 +0000

    Update clock.py

--- clock.py
+++ clock.py
@@ -3,28 +3,20 @@ import time
 import random
 
 w=Tk()
-w.config(bg="blue")
 w.title("Clock app")
 
-r=random.randint(0,255)
-g=random.randint(0,255)
-b=random.randint(0,255)
-
-def randcolour():
-  return f"#{r:02x}{g:02x}{b:02x}"
+def rand_colour():
+  return f"#{random.randint(0,255):02x}{random.randint(0,255):02x}{random.randint(0,255):02x}"
 
 def clock():
   current_time=time.strftime("%H:%M:%S %p")
   l1.config(text=current_time)
-  if int(time.strftime("%S"))%2==0:
-    l1.config(bg=randcolour())
-  else:
-    l1.config(bg=randcolour())
+  l1.config(fg=rand_colour(),bg=rand_colour())
   l1.after(1000,clock)
   
 
 
-l1=Label(w,bg="blue",font=("calibri",25)) 
+l1=Label(w,font=("calibri",25)) 
 l1.pack()
 clock()
 
